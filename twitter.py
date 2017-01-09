249f0b2b8965af486e34c39da96732d6dd187144
root <root@debian>
2017-01-09 23:10:01 +0530

    "updated twitter"

--- twitter.py
+++ twitter.py
@@ -1,16 +1,32 @@
 import tweepy
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+
+
 
 auth = tweepy.OAuthHandler("4OAW8cfY6FLy1xW8LhLJwzMMp","Bz70UwAIPVzOV3j6lIqUjhSgbLY9GCAuy8jUIjcsyvKwMMP91p")
 auth.set_access_token("358637575-qNNpnSRvnPOXZeafmwedWcL1VKw0DD9pBYwKpxRg","j4xYlBkQJtBOFlnAFAY8HdDEfyriJCpvxHmgR3WZQ94ew")
 
-api = tweepy.API(auth)
-
-public_tweets = api.home_timeline()
-#for tweet in public_tweets:
-#    print tweet.text
-user = api.get_user('twitter')
-#print user.screen_name
-print user.followers_count
-for friend in user.friends():
-   print friend.screen_name
+ 
+class MyListener(StreamListener):
+    def on_data(self, data):
+        try:
+            #with open('python.json', 'a') as f:
+            #    f.write(data["text"])
+            #    return True
+            #json_load = json.loads(data)
+            #texts = json_load['text']
+            print data.text
+        except BaseException as e:
+            #print &quot;Error on_data: %s&quot; % str(e))
+            pass
+        return True
+ 
+    def on_error(self, status):
+        print(status)
+        return True
+ 
+twitter_stream = Stream(auth, MyListener())
+twitter_stream.filter(track=['#python'])
 
